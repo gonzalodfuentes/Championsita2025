4b3770f162439e53844718f78a5875c111b13fc7
JuanerArg <juaniferrando2007@gmail.com>
2025-07-21 10:20:58 -0300

    chore: iniciar rama develop

--- core/src/main/java/com/championsita/name/Principal.java
+++ core/src/main/java/com/championsita/name/Principal.java
@@ -6,16 +6,19 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Principal extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private FitViewport viewport;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        viewport = new FitViewport(8, 5);
     }
 
     @Override
@@ -25,6 +28,11 @@ public class Principal extends ApplicationAdapter {
         batch.draw(image, 140, 210);
         batch.end();
     }
+    
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
+    }	
 
     @Override
     public void dispose() {
