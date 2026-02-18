f1fdfbe807788a11231726c8a9f887a146563654
gonzalodfuentes <130400825+gonzalodfuentes@users.noreply.github.com>
2026-02-18 14:49:16 -0300

    Add files via upload

---
+++ src/main/java/com/championsita/championsita/modelo/Arco.java
@@ -0,0 +1,34 @@
+package com.championsita.modelo;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class Arco {
+
+    private Rectangle arcoSize;
+
+    public Arco(float x, float y, float ancho, float alto) {
+        arcoSize = new Rectangle(x, y, ancho, alto);
+    }
+
+    public Rectangle getHitbox() {
+        return arcoSize;
+    }
+
+    public float getX() {
+        return arcoSize.getX();
+    }
+    public float getY() {
+        return arcoSize.getY();
+    }
+    public float getWidth() {
+        return arcoSize.getWidth();
+    }
+    public float getHeight() {
+        return arcoSize.getHeight();
+    }
+
+
+
+
+
+}
